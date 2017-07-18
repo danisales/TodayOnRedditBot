b583af7c3e7655847e4505920047b4b5e3c36bff
Danielle Sales <dso@cin.ufpe.br>
2017-07-18 00:11:49 -0300

    Update commands so user can choose subreddit and number of posts returned

--- bot.py
+++ bot.py
@@ -17,25 +17,60 @@ def start(bot, update):
 	update.message.reply_text('Starting bot...')
 
 def help(bot, update):
-	msg = '/get_top_posts: return today\'s top 5 /r/all posts\n'
-	msg +='/get_popular_posts: return popular posts from /r/all'
+	msg = '/get_top_posts <subreddit> <number of posts> (e.g. /get_top_posts AskReddit 3): return today\'s top subreddit posts\n\n'
+	msg +='/get_popular_posts <subreddit> <number of posts> (e.g. /get_popular_posts AskReddit 3): return popular posts from chosen subreddit\n\n'
+	msg +='Default subreddit: /r/all\nDefault number of posts: 5'
 	update.message.reply_text(msg)
 
-def get_top_posts(bot, update):
-	for submission in reddit.subreddit('all').top('day', limit=5):
-		msg = ''
-		msg += submission.title + '\n'
-		msg += submission.url + '\n'
-		msg += submission.shortlink + '\n\n'
-		update.message.reply_text(msg)
+def get_top_posts(bot, update, args):
+	subreddit = 'all'
+	limit = 5
+	
+	if(len(args) != 0):
+		subreddit = args[0]
+	if(len(args) == 2):
+		try:
+			limit = int(args[1])
+		except:
+			error = 'Ops, something went wrong :(\n'
+			error += 'Usage: /get_top_posts <subreddit> <number of posts>'
+			update.message.reply_text(error)
+	
+	try:
+		for submission in reddit.subreddit(subreddit).top('day', limit=limit):
+			msg = submission.title + '\n'
+			msg += submission.url + '\n'
+			msg += submission.shortlink
+			update.message.reply_text(msg)
+	except:
+		error = 'Ops, something went wrong :(\n'
+		error += 'Usage: /get_top_posts <subreddit> <number of posts>'
+		update.message.reply_text(error)
 
-def get_popular_posts(bot, update):
-	for submission in reddit.subreddit('all').hot(limit=5):
-		msg = ''
-		msg += submission.title + '\n'
-		msg += submission.url + '\n'
-		msg += submission.shortlink + '\n\n'
-		update.message.reply_text(msg)
+def get_popular_posts(bot, update, args):
+	subreddit = 'all'
+	limit = 5
+
+	if(len(args) != 0):
+		subreddit = args[0]
+	if(len(args) == 2):
+		try:
+			limit = int(args[1])
+		except:
+			error = 'Ops, something went wrong :(\n'
+			error += 'Usage: /get_popular_posts <subreddit> <number of posts>'
+			update.message.reply_text(error)
+
+	try:
+		for submission in reddit.subreddit(subreddit).hot(limit=limit):
+			msg = submission.title + '\n'
+			msg += submission.url + '\n'
+			msg += submission.shortlink
+			update.message.reply_text(msg)
+	except:
+		error = 'Ops, something went wrong :(\n'
+		error += 'Usage: /get_popular_posts <subreddit> <number of posts>'
+		update.message.reply_text(error)
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -46,8 +81,8 @@ def main():
 
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("help", help))
-	dp.add_handler(CommandHandler("get_top_posts", get_top_posts))
-	dp.add_handler(CommandHandler("get_popular_posts", get_popular_posts))
+	dp.add_handler(CommandHandler("get_top_posts", get_top_posts, pass_args=True))
+	dp.add_handler(CommandHandler("get_popular_posts", get_popular_posts, pass_args=True))
 
 	updater.start_polling()
 	updater.idle()
