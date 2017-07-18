ae6e06cd4a67e6fe3f47a68bdd920607a62e30dc
Danielle Sales <dso@cin.ufpe.br>
2017-07-18 01:45:44 -0300

    Add error handler

--- bot.py
+++ bot.py
@@ -81,6 +81,8 @@ def main():
 	dp.add_handler(CommandHandler("get_top_posts", get_top_posts, pass_args=True))
 	dp.add_handler(CommandHandler("get_popular_posts", get_popular_posts, pass_args=True))
 
+	dp.add_error_handler(error)
+	
 	updater.start_polling()
 	updater.idle()
 
