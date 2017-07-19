e68dde3076fd2f341f0f3c4ba646d01d415e6801
Danielle Sales <dso@cin.ufpe.br>
2017-07-19 18:25:37 -0300

    Update scheduled message

--- scheduler.py
+++ scheduler.py
@@ -17,16 +17,20 @@ users = db['users']
 
 bot = Bot(config.token)
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, timezone='America/Bahia')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, timezone='America/Bahia')
 def scheduled_job():
 	for user in users.find({'notification': True}):
 		subreddits = user['subreddits']
 		if(len(subreddits) > 0):
 			for subreddit in subreddits:
+				bot.send_message(user['id'], text='Today on ' + subreddit)
 				for submission in reddit.subreddit(subreddit).hot(limit=user['nb_posts']):
 					msg = submission.title + '\n'
 					msg += submission.url + '\n'
 					msg += submission.shortlink
 					bot.send_message(user['id'], text=msg)
+			msg = 'You are receiving this messages because you turned notification on. '
+			msg += 'If you no longer want to receive this, just send /set_notification.'
+			bot.send_message(user['id'], msg)
 
 sched.start()
